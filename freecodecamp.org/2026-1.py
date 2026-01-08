b8a3e6a331d9caa2793d96ce9218aac24f0bd4a3
miscroV <miscroV@outlook.com>
2026-01-08 15:00:23 +0000

    split testing function from Month. and docs and comment out unit testing

--- freecodecamp.org/2026-1.py
+++ freecodecamp.org/2026-1.py
@@ -1,25 +1,16 @@
 # Started on Day 8
-def testFunc(func, cases: list, results:list) -> bool:
-    """
-    @brief runs the function with the provided case and result set and returns a success/fail value
-    
-    @param func, the function for testing
-    @param cases, a list of the cases for a simple function
-    @param results, a list of the return values the function should return for the corresponding case
-    
-    @return boolean value if the function passed all test cases. 
-    """
-    print("// running tests")
-    Passed = True
-    for case, result in zip(cases, results):
-        if func(case) != result:
-            Passed = False
-            print(f"{func.__name__}({case}) should return {result}")
-    print("// tests complted")
-    return Passed
-
+from testing import testFunc
 
 def is_sorted(arr):
+    """ Jan-8
+    Given an array of numbers, determine if the numbers are sorted in ascending order, descending order, or neither.
+
+    If the given array is:
+
+    In ascending order (lowest to highest), return "Ascending".
+    In descending order (highest to lowest), return "Descending".
+    Not sorted in ascending or descending order, return "Not sorted".
+    """
     is_acc = True
     is_des = True
     for i in range(1, len(arr)):
@@ -32,24 +23,14 @@ def is_sorted(arr):
         return "Descending"
     else:
         return "Not sorted"
-    
 
-            
-cases = [
-    [1,2,3,4,5],
-    [10,8,6,4,2],
-    [1,3,2,4,5],
-    [3.14, 2.71, 1.61, 0.57],
-    [12.3, 23.4, 34.5, 45.6, 56.7, 67.8, 78.9],
-    [0.4, 0.5, 0.3]
-]
-results =[
-    "Ascending",
-    "Descending",
-    "Not sorted",
-    "Descending",
-    "Ascending",
-    "Not sorted"
-]
-
-if testFunc(is_sorted, cases, results): print(f"{is_sorted.__name__} passed testing")
+# UNIT TESTING      
+# cases = [
+#     [1,2,3,4,5],[10,8,6,4,2],[1,3,2,4,5],[3.14, 2.71, 1.61, 0.57],
+#     [12.3, 23.4, 34.5, 45.6, 56.7, 67.8, 78.9],[0.4, 0.5, 0.3]
+# ]
+# results =[
+#     "Ascending","Descending","Not sorted","Descending",
+#     "Ascending","Not sorted"
+# ]
+# if testFunc(is_sorted, cases, results): print(f"{is_sorted.__name__} passed testing")
