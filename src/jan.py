c69db4eaba3e675bc2332e1e7a76ce98f573d94f
miscroV <miscroV@outlook.com>
2026-01-10 10:06:50 +0000

    revert

--- src/jan.py
+++ src/jan.py
@@ -97,9 +97,12 @@ def tic_tac_toe(board):
     views += [[board[i][k] for i,k  in 
                zip(range(0,len(board)),range(len(board)-1,-1,-1))]] # Diagonal 2
     
-    wins   = [view[0] if all(cell == view[0] for cell in view) 
-              else None for view in views]
-    winner = [win for win in wins if win is not None][0]
+    winner = None
+    for view in views:
+        if all(cell == view[0] for cell in view):
+            winner = view[0]
+            break;
+    
 
     return f"{winner} wins" if winner != None else "Draw"
 
