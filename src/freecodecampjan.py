82450828f16c566d549fa436aa5eaee696a3ca3a
miscroV <miscroV@outlook.com>
2026-01-18 19:06:04 +0000

    chessboard array

--- src/freecodecampjan.py
+++ src/freecodecampjan.py
@@ -242,3 +242,54 @@ def is_integer_hypotenuse(a: int, b: int) -> bool:
     h = math.sqrt((a**2) + (b**2))
 
     return True if h == int(h) else False
+
+def knight_moves(position: str) -> int:
+    """
+    Given the position of a knight on a chessboard, return the number of valid squares the knight can move to.
+
+    Parameters
+    ----------
+    postion: str
+        A position on a chess board in chess notation, e.g. "A8" or "H1"
+    
+    Returns
+    -------
+    int
+        The number of valid moves for a knight located on that square. 
+
+    Notes
+    -----  
+    A standard chessboard is 8x8, with columns labeled A through H (left to right) and rows labeled 1 through 8 (bottom to top). It looks like this:
+    
+    A8 	B8 	C8 	D8 	E8 	F8 	G8 	H8
+
+    A7 	B7 	C7 	D7 	E7 	F7 	G7 	H7
+
+    A6 	B6 	C6 	D6 	E6 	F6 	G6 	H6
+
+    A5 	B5 	C5 	D5 	E5 	F5 	G5 	H5
+
+    A4 	B4 	C4 	D4 	E4 	F4 	G4 	H4
+
+    A3 	B3 	C3 	D3 	E3 	F3 	G3 	H3
+
+    A2 	B2 	C2 	D2 	E2 	F2 	G2 	H2
+
+    A1 	B1 	C1 	D1 	E1 	F1 	G1 	H1
+
+    A knight moves in an "L" shape: two squares in one direction (horizontal or vertical), and one square in the perpendicular direction.
+
+    This means a knight can move to up to eight possible positions, but fewer when near the edges of the board. For example, if a knight was at A1, it could only move to B3 or C2.
+
+    """
+    
+
+    # movements +/-2 Letter and +/-1 number or +/-1 letter and +/-2 numbers
+
+    chessboard = [[f"{l}{n}" for n in range(8,0,-1)] for l in "ABCDEFGH"]
+
+    print(chessboard)
+
+knight_moves("A1")
+
+
