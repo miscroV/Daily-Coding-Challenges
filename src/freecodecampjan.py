2155c5449ff499771190847814fef3106cee66aa
miscroV <miscroV@outlook.com>
2026-01-19 20:59:46 +0000

    Solve challenge and write docs

--- src/freecodecampjan.py
+++ src/freecodecampjan.py
@@ -339,3 +339,37 @@ def gets_free_shipping(cart: list[str], minimum: float) -> bool:
     for item in cart: total += item_costs.get(item, 0)
     return total > minimum
 
+def compare_energy(calories_burned: int, watt_hours_used: int) -> str:
+    """
+    Given the number of Calories burned during a workout, and the number of watt-hours used by your electronic devices during that workout, determine which one used more energy.
+
+    Parameters
+    ----------
+    calories_burned: int
+        Number of calories burned during workout
+    watt_hours_used: int
+        Number of watt hours used by electronic devices during workout. 
+
+    Returns
+    -------
+    str:
+        string refering to the higher value between calories burned and what hours used. Can also return "Equal".
+
+    Notes
+    -----
+    To compare them, convert both values to joules using the following conversions:
+
+    1 Calorie equals 4184 joules.
+
+    1 watt-hour equals 3600 joules.
+
+    Return:
+
+    "Workout" if the workout used more energy.
+
+    "Devices" if the device used more energy.
+
+    "Equal" if both used the same amount of energy.
+    """
+    cals_in_J, watts_in_J = calories_burned * 4184, watt_hours_used * 3600
+    return "Equal" if cals_in_J == watts_in_J else ("Workout" if cals_in_J > watts_in_J else "Devices")
